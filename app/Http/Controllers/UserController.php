612cc0347529eb63dd8754587cfc4b20412e2e8f
Junior Rodrigues <spider_junior01@hotmail.com>
2021-10-19 02:34:49 -0300

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,32 +26,4 @@ class UserController extends Controller
     {
         return response()->json(['user' => Auth::user()], 200);
     }
-
-    /**
-     * Get all User.
-     *
-     * @return Response
-     */
-    public function allUsers()
-    {
-         return response()->json(['users' =>  User::all()], 200);
-    }
-
-    /**
-     * Get one user.
-     *
-     * @return Response
-     */
-    public function singleUser($id)
-    {
-        try {
-            $user = User::findOrFail($id);
-
-            return response()->json(['user' => $user], 200);
-
-        } catch (\Exception $e) {
-
-            return response()->json(['message' => 'Usuário não encontrado!'], 404);
-        }
-    }
 }
